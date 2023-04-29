d49b0544d6c8de9f4197c5bd2fcaff64f366fb87
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-04-29 21:36:33 +0100

    Fix new-version.sh

--- retrofit/new-version.sh
+++ retrofit/new-version.sh
@@ -6,7 +6,7 @@ OLDVERSION=$1
 CONFIGFILE=$(lowarn-cli locate)
 PROJECTDIR=$(dirname "$CONFIGFILE")
 
-COMMITID=$(sed "$((OLDVERSION + 1))q;d" "$PROJECTDIR/.lowarn-retrofit/*/commit-map")
+COMMITID=$(sed "$((OLDVERSION + 1))q;d" "$PROJECTDIR"/.lowarn-retrofit/*/commit-map)
 
 cd "$PROJECTDIR/versions"
 
@@ -14,7 +14,7 @@ NEWVERSION=$(find . -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | sort | head
 
 lowarn-cli retrofit version --version "$OLDVERSION" apply source
 
-CABALVERSION=$(awk '/^version:\s+(.+)/{ print $2 }' "./$OLDVERSION/source/xmonad.cabal")
+CABALVERSION=$(awk '/^version:\s+(.+)/{ print $2 }' ./"$OLDVERSION"/source/*.cabal)
 
 cp "./$NEWVERSION/simplify.patch" "./$OLDVERSION/simplify.patch"
 sed -i -e "s/$NEWVERSION/$OLDVERSION/g;" "./$OLDVERSION/simplify.patch"
